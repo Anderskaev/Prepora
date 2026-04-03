a962e8c97a9f4f10e8998ade67b07261f4bf0fd8
Vlad <v.anderskaev@gmail.com>
2026-04-03 16:50:11 +0300

    Update app.config.ts

--- src/app/app.config.ts
+++ src/app/app.config.ts
@@ -36,7 +36,7 @@ export const appConfig: ApplicationConfig = {
     
     provideTranslateService({
       loader: provideTranslateHttpLoader({
-        prefix: environment.langHref,
+        prefix: 'langs/',
         suffix: '.json'
       }),
       fallbackLang: 'ru',
