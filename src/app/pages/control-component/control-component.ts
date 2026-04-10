c121c05956cf72d94e682468cd971cd8ceb58b99
Vlad <v.anderskaev@gmail.com>
2026-04-10 19:47:55 +0300

    Update control-component.ts

--- src/app/pages/control-component/control-component.ts
+++ src/app/pages/control-component/control-component.ts
@@ -74,7 +74,7 @@ export class ControlComponent {
   save() {
     if (!this.formTitle().trim() || !this.formDate()) return;
     const existing = this.editItem();
-    this.formDate()!.setHours(19, 25, 0, 0); // Ставим полдень для единообразия
+    this.formDate()!.setHours(20, 0, 0, 0); 
     const payload = {
       title:  this.formTitle().trim(),
       date:   this.formDate()!.toISOString(),
