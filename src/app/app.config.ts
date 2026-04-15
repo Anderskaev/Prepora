97c4ff263d3e17f601c7d82848a46b71e82ad309
Vlad <v.anderskaev@gmail.com>
2026-04-15 17:09:32 +0300

    Update app.config.ts

--- src/app/app.config.ts
+++ src/app/app.config.ts
@@ -31,7 +31,7 @@ export const appConfig: ApplicationConfig = {
                 preset: MyPreset,
                    options: {
                      // darkModeSelector: '.my-app-dark'
-                     //darkModeSelector: false
+                     darkModeSelector: false
                     }                
             }
     }),
